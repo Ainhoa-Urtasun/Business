c3dd6cfac6a19a513b4bad5747684da1d8b28938
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-08-16 09:16:49 +0200

    Update fs.py

--- fs.py
+++ fs.py
@@ -1,3 +1,14 @@
+global Tangible_fixed_assets
+global Accumulated_depreciation
+global Stocks
+global Debtors
+global Cash
+global Capital
+global Revenue
+global Expenses
+global Dividends
+global Noncurrent_liabilities
+global Current_liabilities
 Fixed_assets = Tangible_fixed_assets + Accumulated_depreciation
 Current_assets = Stocks + Debtors + Cash
 Retained_earnings = Revenue + Expenses + Dividends
