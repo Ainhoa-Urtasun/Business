e8d905f6068bd85f75b074b566b48226e93b1bd9
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-08-16 09:16:26 +0200

    Update values.py

--- values.py
+++ values.py
@@ -1,15 +1,3 @@
-global Tangible_fixed_assets
-global Accumulated_depreciation
-global Stocks
-global Debtors
-global Cash
-global Capital
-global Revenue
-global Expenses
-global Dividends
-global Noncurrent_liabilities
-global Current_liabilities
-
 Tangible_fixed_assets = 0
 Accumulated_depreciation = 0
 Stocks = 0
