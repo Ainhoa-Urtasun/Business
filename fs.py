0c348f1112b773ecc6d9f9afc240f837f44f588c
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-08-16 08:58:41 +0200

    Update fs.py

--- fs.py
+++ fs.py
@@ -1,17 +1,5 @@
 import matplotlib.pyplot as plt
 
-Tangible_fixed_assets = 0
-Accumulated_depreciation = 0
-Stocks = 0
-Debtors = 0
-Cash = 0
-Capital = 0
-Revenue= 0
-Expenses = 0
-Dividends = 0
-Noncurrent_liabilities = 0
-Current_liabilities = 0
-
 global Tangible_fixed_assets
 global Accumulated_depreciation
 global Stocks
@@ -23,6 +11,18 @@ global Expenses
 global Dividends
 global Noncurrent_liabilities
 global Current_liabilities
+
+Tangible_fixed_assets = 0
+Accumulated_depreciation = 0
+Stocks = 0
+Debtors = 0
+Cash = 0
+Capital = 0
+Revenue= 0
+Expenses = 0
+Dividends = 0
+Noncurrent_liabilities = 0
+Current_liabilities = 0
 
 def SFP():
   Fixed_assets = Tangible_fixed_assets + Accumulated_depreciation
