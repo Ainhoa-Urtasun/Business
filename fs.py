7275fd26e12c577c54eddfdbaa22679e9af14441
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-08-16 11:43:45 +0200

    Update fs.py

--- fs.py
+++ fs.py
@@ -12,7 +12,7 @@ Dividends = 0
 Noncurrent_liabilities = 0
 Current_liabilities = 0
 
-def sfp(Capital,Cash):
+def sfp(Tangible_fixed_assets,Accumulated_depreciation,Stocks,Debtors,Cash,Capital,Revenue,Expenses,Dividends,Noncurrent_liabilities,Current_liabilities):
   Fixed_assets = Tangible_fixed_assets + Accumulated_depreciation
   Current_assets = Stocks + Debtors + Cash
   Retained_earnings = Revenue + Expenses + Dividends
