7ad81bc860127b106b9a07745a1e561b078e948a
Ainhoa <ainhoa.urtasun@unavarra.es>
2022-09-28 10:33:39 +0200

    Update fs.py

--- fs.py
+++ fs.py
@@ -46,6 +46,8 @@ def Income(Revenue,COGS,Other_operating_expenses,Financial_expenses,Taxation,Gro
   plt.bar(labels,[0,-Other_operating_expenses],bottom=[Revenue,-Taxation-Financial_expenses],color=['green','violet'])
   plt.bar(labels,[0,-COGS],bottom=[Revenue,-Taxation-Financial_expenses-Other_operating_expenses],color=['green','purple'])
   plt.hlines(0,-1,3,colors='red',linestyle='--')
+  plt.xtick([])
+  plt.text(2,0,'Expenses')
   plt.title('Income Statement')
   if Revenue > 0:
     plt.text(0,Revenue*0.5,'Revenue\n'+str(Revenue),va='center',ha='center')
